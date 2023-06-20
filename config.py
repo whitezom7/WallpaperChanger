1299b48a94d8820787b5c968a2888d73d4c8a80e
Taylor <contact@whitewood.dev>
2023-06-20 23:57:02 +0200

    Update

--- config.py
+++ config.py
@@ -1,8 +1,12 @@
+# Enter Your API Key here from https://wallhaven.cc/settings/account, will need to create an account first
+ApiKey="" 
 
-ApiKey = "rPd7LjmXtplCDgJyk7oJoi7kSIbVyUAh" # Enter Your API Key here from https://wallhaven.cc/settings/account, will need to create an account first
+# Enter a search query/tag here to fetch wallpapers from
+Query="" 
 
-Query = "Furry" # Enter a search query/tag here to fetch wallpapers from
+# 100* /110/111/etc (sfw/sketchy/nsfw) Turn purities on(1) or off(0) 
+# NSFW requires a valid API key, ( Etc "100" is SFW, "010" is Sketchy, "001" is NSFW)
+PurityLevel="" 
 
-PurityLevel = "001" # 100* /110/111/etc (sfw/sketchy/nsfw) Turn purities on(1) or off(0) NSFW requires a valid API key, ( Etc "100" is SFW, "010" is Sketchy, "001" is NSFW)
-
-KeyBind = "ctrl+alt+w" # default is ctrl+alt+w, use https://github.com/boppreh/keyboard#api to see all different key names
+# Default is ctrl+alt+w, use https://github.com/boppreh/keyboard#api to see all different key names
+KeyBind="ctrl+alt+w" 
